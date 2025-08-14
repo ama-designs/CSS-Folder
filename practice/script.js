915b127eafe88a8979f54b99d126a649a0930169
ama-designs <amalina.designss@gmail.com>
2025-08-14 12:09:15 +0100

    upload files for redeploy

---
+++ practice/script.js
@@ -0,0 +1,50 @@
+// $('#search').focusin(function () {
+//     $('#Modal').show();
+// });
+
+// $('#close').click(function () {
+//     $('#Modal').hide();
+// });
+
+// Day 12
+
+// let search= document.getElementById('search');
+// let button= document.getElementById('button');
+// let input= document.getElementById('input');
+
+// function loading() {
+//     search.classList.add('loading');
+
+//     setTimeout(function () {
+//         search.classList.remove('loading');
+//     }, 1500);
+// }
+
+// button.addEventListener('click', loading);
+
+
+// input.addEventListener('keydown', function() {
+//     if(event.keyCode ==13) loading();
+// })
+
+
+// Day 13
+
+// const signInBtn = document.getElementById('signIn');
+// const signInBtn = document.getElementById('signUp');
+// const firstForm = document.getElementById('form1');
+// const secondForm = document.getElementById('form2');
+// const container = document.querySelector('container');
+
+// signInBtn.addEventListener('click', () => {
+//     container.classList.remove('right-panel-active');
+// });
+
+// signUpBtn.addEventListener('click', () => {
+//     container.classList.add('right-panel-active');
+// });
+
+
+// firstForm.addEventListener('submit', (e) => e.preventDefault());
+// secondForm.addEventListener('submit', (e) => e.preventDefault());
+
